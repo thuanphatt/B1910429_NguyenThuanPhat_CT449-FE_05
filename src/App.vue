6cbf55e7bc3aebdcb36a5c2acfd74afd552ed6d0
thuanphatt <zelorntp@gmail.com>
2023-03-19 21:15:12 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="page">Hello, Vue.js</h1>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
