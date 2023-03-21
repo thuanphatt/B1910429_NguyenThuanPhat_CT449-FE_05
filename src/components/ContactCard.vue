e4f45a727d19d658c089e93356ba9a1ca28bf0d9
thuanphatt <zelorntp@gmail.com>
2023-03-21 08:15:21 +0700

    cap nhat va sua loi icon

--- src/components/ContactCard.vue
+++ src/components/ContactCard.vue
@@ -18,8 +18,24 @@
     </div>
     <div class="p-1">
       <strong>Liên hệ yêu thích:&nbsp;</strong>
-      <i v-if="contact.favorite" class="fas fa-check"></i>
-      <i v-else class="fas fa-times"></i>
+      <svg
+        v-if="contact.favorite"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+      >
+        <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+        <path
+          fill="#fff"
+          d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
+        />
+      </svg>
+      <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+        <!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+        <path
+          fill="#fff"
+          d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"
+        />
+      </svg>
     </div>
   </div>
 </template>
